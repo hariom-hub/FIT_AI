37eee711311518da1c66f7eb5d0648d8afb7810c
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-09-10 17:49:17 +0530

    css folder added

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.set("views engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 //home route
