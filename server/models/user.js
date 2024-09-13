c9ae6dcf75280f164e7919bf17a1c970e0785f2a
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-09-13 19:27:10 +0530

    authentications is about to complete

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const passportlocalMongoose = require("passport-local-mongoose");
+const userSchema = new Schema({
+
+    email : {
+
+        type : String,
+        required : true
+    },
+    //passport js automatically adds user-name
+});
+
+// User.plugin(passportlocalMongoose);
+userSchema.plugin(passportlocalMongoose);
+module.exports = mongoose.model("User",userSchema);
--- server/server.js
+++ server/server.js
@@ -1,34 +1,77 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const mongoose = require("mongoose");
 const path = require("path");
-app.set("views engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 
-//home route
+const mongoUrl = 'mongodb://127.0.0.1:27017/Fit_AI';
+main().then(() => {
 
-app.get("/",(req,res)=>{
+    console.log("db connected successfully");
+}).catch((error) => {
+    console.log(error);
+});
+
+async function main() {
+
+    await mongoose.connect(mongoUrl);
+}
+
+app.use(flash());
+app.use(passport.initialize());
+
+const sessionOptions = {
+    secret: 'SECRET_SESSION_KEY', // Replace with a strong, unique secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to `true` if using HTTPS
+};
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
 
     res.send("<h1>You are on the root path");
+});
+
+
+app.use(session(sessionOptions));
+app.get("/demouser", async (req, res) => {
+
+    let fakeUser = new User({
+        email: "demo@gmail.com",
+        username: "demo-student4"
+    });
+    let  registeredUser  = await User.register(fakeUser, "hello world");
+    res.send(registeredUser);
 })
-app.get("/home",(req,res)=>{
+app.get("/home", (req, res) => {
 
     res.render("home.ejs");
 })
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
 
     res.render("login.ejs");
 })
 
-app.get("/signup",(req,res)=>{
+app.get("/signup", (req, res) => {
 
     res.render("signup.ejs");
 })
-app.listen(port,()=>{
+app.listen(port, () => {
 
     console.log("server is listening to port : 8080");
 })
